d21028b6faac21f7ab76964c3a252545c160107c
Vladyslav Khakov <vladhakov20@gmail.com>
2024-07-12 07:39:22 -0700

    checked with 2 accounts

--- heliusJitoBundler.ts
+++ heliusJitoBundler.ts
@@ -4,26 +4,44 @@ import { Keypair } from '@solana/web3.js';
 import { createMemoInstruction } from '@solana/spl-memo';
 import bs58 from 'bs58';
 
-import secret from './test-ledger/id.json';
+import secretA from './test-ledger/id1.json';
+import secretB from './test-ledger/id2.json';
 
 dotenv.config();
 
-const wallet = Keypair.fromSecretKey(new Uint8Array(secret));
+const walletA = Keypair.fromSecretKey(new Uint8Array(secretA));
+const walletB = Keypair.fromSecretKey(new Uint8Array(secretB));
 const helius = new Helius(process.env.HELIUS_API_KEY || '', process.env.HELIUS_CLUSTER || 'testnet');
 const jitoApiUrl = `${process.env.BLOCK_ENGINE_URL}/api/v1/bundles`;
 
 const sendHeliusJitoBundle = async () => {
   const sTx1 = bs58.encode(
-    (await helius.rpc.createSmartTransaction([createMemoInstruction('Hello, this is sequence 1. To be continued...')], [wallet])).smartTransaction.serialize()
+    (
+      await helius.rpc.createSmartTransaction([createMemoInstruction('Hello, this is sequence 1. last message from Ishymura...')], [walletA])
+    ).smartTransaction.serialize()
   );
   const sTx2 = bs58.encode(
     (
-      await helius.rpc.createSmartTransaction([createMemoInstruction('Hi, this is sequence 2. sending tip <beep> <beep> <beep>')], [wallet])
+      await helius.rpc.createSmartTransaction([createMemoInstruction('Hi, this is sequence 2. sending lyrics via radio channel #12.31')], [walletA])
     ).smartTransaction.serialize()
   );
-  const jitoTipTx = (await helius.rpc.createSmartTransactionWithTip([], [wallet], undefined, 100_000)).serializedTransaction;
+  const sTx3 = bs58.encode(
+    (
+      await helius.rpc.createSmartTransaction([createMemoInstruction('Hi, this is sequence 3. twinkle, twinkle, little star')], [walletA])
+    ).smartTransaction.serialize()
+  );
+  const sTx4 = bs58.encode(
+    (
+      await helius.rpc.createSmartTransaction(
+        [createMemoInstruction("Hi, this is sequence 4. i don't want to be one of them good bye, Issac...")],
+        [walletB],
+        undefined
+      )
+    ).smartTransaction.serialize()
+  );
+  const jitoTipTx = (await helius.rpc.createSmartTransactionWithTip([], [walletA], undefined, 100_000)).serializedTransaction;
 
-  const bundleId = await helius.rpc.sendJitoBundle([sTx1, sTx2, jitoTipTx], jitoApiUrl);
+  const bundleId = await helius.rpc.sendJitoBundle([sTx1, sTx2, sTx3, sTx4, jitoTipTx], jitoApiUrl);
   const statuses = await helius.rpc.getBundleStatuses([bundleId], jitoApiUrl);
 
   console.log('slot:', statuses.context.slot);
